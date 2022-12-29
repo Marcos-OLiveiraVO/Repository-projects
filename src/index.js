b071b996853193b25ad0c4333b99972fce0087e6
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2022-12-29 15:25:26 -0300

    fix: post likes

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json(likes);
+  return response.json({ likes });
 });
 
 module.exports = app;
